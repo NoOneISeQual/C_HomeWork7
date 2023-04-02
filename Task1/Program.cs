3e713bfbca0eca7e99d2ef6eb47efb8d78e182a1
NoOneISeQual <kurdefrance1613@gmail.com>
2023-04-02 17:13:22 +0300

    Add Task1

---
+++ Task1/Program.cs
@@ -0,0 +1,28 @@
+﻿void InputMatrix(double[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0 ; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = Convert.ToDouble(new Random().Next(-100, 100)) / 10; 
+        }
+    }
+}
+
+void PrintMatrix(double[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for(int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write($"{matrix[i, j]} \t");
+        }
+        Console.WriteLine();
+    }
+}
+Console.Clear();
+Console.WriteLine("Введите размерность двумерного массива через пробел: ");
+int[] size = Console.ReadLine()!.Split().Select(x => int.Parse(x)).ToArray();
+double[,] matrixnumbers = new double[size[0], size[1]];
+InputMatrix(matrixnumbers);
+PrintMatrix(matrixnumbers);
